668df4b27f5449042388e9c70adb4d8da1ed8d64
pwxcoo <1050270321@qq.com>
2018-12-18 00:03:02 +0800

    leetcode960

---
+++ 2018-12/2018-12-17/leetcode960.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& A) {
+        int m = A.size(), n = A[0].length(), res = n - 1, k;
+        vector<int>dp(n,1);
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < j; ++i) {
+                for (k = 0; k < m; ++k) {
+                    if (A[k][i] > A[k][j]) break;
+                }
+                if (k == m && dp[i] + 1 > dp[j])
+                    dp[j] = dp[i] + 1;
+            }
+            res = min(res, n - dp[j]);
+        }
+        return res;
+    }
+};
